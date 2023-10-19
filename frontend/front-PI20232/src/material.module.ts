5456dc1e9aedb834d8eaeb98b27dbde325f2ed99
7rik <bezerra.erik2003@gmail.com>
2023-10-19 17:14:40 -0300

    criado painel de carros e estilização melhorada

--- frontend/front-PI20232/src/material.module.ts
+++ frontend/front-PI20232/src/material.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from "@angular/core";
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
 import { CommonModule } from '@angular/common';
 
 @NgModule({
@@ -9,7 +13,11 @@ import { CommonModule } from '@angular/common';
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
-    CommonModule
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    MatChipsModule
   ]
 })
 export class MaterialModule{}
